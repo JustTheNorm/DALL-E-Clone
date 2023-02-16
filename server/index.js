36879acee4f63801bc27f2a453ddb775a9473634
Norman <whosnorm@gmail.com>
2023-02-15 22:27:40 -0500

    added dotenv again

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import dalleRoutes from './routes/dalleRoutes.js';
 
-
+const dotenv = require('dotenv')
 dotenv.config();
 
 const app = express();
